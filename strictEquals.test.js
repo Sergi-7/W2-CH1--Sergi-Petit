14a55b58b901735e476608b6efe86cc0d152ca92
Sergi Petit <sergi_hak_7@hotmail.com>
2021-10-05 18:04:37 +0200

    Añadir primer test

--- strictEquals.test.js
+++ strictEquals.test.js
@@ -1,3 +1,16 @@
 function strictEquals(a, b) {
-  Object.is(a, b);
+  return Object.is(a, b);
 }
+
+test("If a is 1, and b is 1 , it should return true", () => {
+  //Arrange
+  const a = 1;
+  const b = 1;
+  const expected = true;
+
+  //Act
+  const result = strictEquals(a, b);
+
+  //Assert
+  expect(result).toBe(expected);
+});
